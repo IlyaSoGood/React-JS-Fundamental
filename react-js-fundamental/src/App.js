5648e6fea97f9d2df56700f0b8b64e431b8542d1
Ilya Ermolin <tomixlove@gmail.com>
2023-05-18 16:47:15 +0300

    Добавление/удаление постов, сортировка через управляемые инпуты, UI компоненты через css модули

--- react-js-fundamental/src/App.js
+++ react-js-fundamental/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './styles/App.css';
+import PostList from './components/PostList';
+import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'ааа', body: 'бб'},
+        {id: 2, title: 'ггг 2', body: 'аа'},
+        {id: 3, title: 'ввв 3', body: 'яя'}
+    ])
+
+    function getSortedPosts() {
+        console.log('ОТРАБОТАЛА ФУНКЦИЯ SORTED POSTS ')
+        if(selectedSort) {
+            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+        }
+        return posts;
+    }
+    
+    const [selectedSort, setSelectedSort] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const sortedPosts = getSortedPosts();
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+    //Получаем пост из дочернего компонента
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort)
+    }
+
+    return (
+        <div className="App">
+            <PostForm create={createPost}/>
+            <hr style={{margin: "15px 0"}}/>
+            <div>
+                <MyInput
+                    value={searchQuery}
+                    onChange={e => setSearchQuery(e.target.value)}
+                    placeholder="поиск..."
+                />
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue="Сортировка"
+                    options={[
+                        {value: 'title', name: 'По названию'},
+                        {value: 'body', name: 'По описанию'}
+                    ]}
+                />
+            </div>
+
+            {posts.length !== 0
+                ? <PostList remove={removePost} posts={sortedPosts} title={'Посты про JS'}/>
+                : <h1 style={{textAlign: 'center'}}>Посты не были найдены</h1>
+            }
+        </div>
+    );
 }
 
 export default App;
