00bd69ab10869660902062608c42ac4241148793
Ilya Ermolin <tomixlove@gmail.com>
2023-05-26 15:44:40 +0300

    React transition group, Кастомные хуки, пагинация через useEffect, axios, открытие PostForm в модалке

--- react-js-fundamental/src/App.js
+++ react-js-fundamental/src/App.js
@@ -1,70 +1,91 @@
-import React, {useMemo, useState} from 'react';
+import React, {useMemo, useState, useEffect} from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
-import MySelect from './components/UI/select/MySelect';
-import MyInput from './components/UI/input/MyInput';
+import PostFiler from './components/PostFiler';
+import MyModal from './components/UI/MyModal/MyModal';
+import MyButton from './components/UI/button/MyButton';
+import { usePosts } from './hooks/usePost';
+import PostService from './API/PostService';
+import Loader from './components/UI/Loader/Loader';
+import { useFetching } from './hooks/useFetching';
+import { getPagesCount } from './utils/pages';
+
+// import {getPagesArray} from './utils/pages';
+import { usePagination } from './hooks/usePagination';
+
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'ааа', body: 'бб'},
-        {id: 2, title: 'ггг 2', body: 'аа'},
-        {id: 3, title: 'ввв 3', body: 'яя'}
-    ])
-   
-    const [selectedSort, setSelectedSort] = useState('');
-    const [searchQuery, setSearchQuery] = useState('');
+    const [posts, setPosts] = useState([])
+    const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
-    const sortedPosts = useMemo(() => {
-        console.log('ОТРАБОТАЛА ФУНКЦИЯ СОРТЕД ПОСТС')
-        if(selectedSort) {
-            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
-        }
-        return posts;
-    }, [selectedSort, posts])
 
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
-    }, [searchQuery, sortedPosts])
+    //ДЗ сделать usePagination
+    let pagesArray = usePagination(totalPages);
+
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const response = await PostService.getAll(limit, page);
+        setPosts(response.data);
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPagesCount(totalCount, limit));
+    })
 
     const createPost = (newPost) => {
-        setPosts([...posts, newPost])
+        setPosts([...posts, newPost]);
+        setModal(false);
     }
+
+    useEffect(() => {
+        fetchPosts()
+    }, [page])
 
     //Получаем пост из дочернего компонента
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
-    const sortPosts = (sort) => {
-        setSelectedSort(sort)
+    const changePage = (page) => {
+        setPage(page);
+        // fetchPosts();
     }
 
     return (
         <div className="App">
-            <PostForm create={createPost}/>
-            <hr style={{margin: "15px 0"}}/>
-            <div>
-                <MyInput
-                    value={searchQuery}
-                    onChange={e => setSearchQuery(e.target.value)}
-                    placeholder="поиск..."
-                />
-                <MySelect
-                    value={selectedSort}
-                    onChange={sortPosts}
-                    defaultValue="Сортировка"
-                    options={[
-                        {value: 'title', name: 'По названию'},
-                        {value: 'body', name: 'По описанию'}
-                    ]}
-                />
-            </div>
+            <button onClick={fetchPosts}>GET POSTS</button>
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+                Создать пользователя
+            </MyButton>
+ 
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>   
 
-            {posts.length !== 0
-                ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Посты про JS'}/>
-                : <h1 style={{textAlign: 'center'}}>Посты не были найдены</h1>
+            <hr style={{margin: "15px 0"}}/>
+            <PostFiler filter={filter} setFilter={setFilter}/>
+
+            {postError &&
+                <h1>Произошла ошибка {postError}</h1>
             }
+            {isPostsLoading
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
+                : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Посты про JS'}/>
+            }
+            <div className="page__wrapper">
+                {pagesArray.map(p =>
+                    <span
+                        onClick={() => changePage(p)}
+                        key={p} 
+                        className={page === p ? 'page page__current' : 'page'}
+                    >
+                        {p}
+                    </span>
+                )}
+            </div>
         </div>
     );
 }
